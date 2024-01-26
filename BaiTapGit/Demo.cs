2a281397bc1e58e1ce8588d55d05d86117453cfe
luaan <luandophu8114@gmail.com>
2024-01-26 16:45:28 +0700

    first commit

---
+++ BaiTapGit/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapGit
+{
+    internal class Demo
+    {
+        int x = 1;
+        int y = 2;
+        int z = 3;
+    }
+}
